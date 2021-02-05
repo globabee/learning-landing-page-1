007b1ffa82f6369be97400fdecc13d3586830e57
Globabee <globabee@lowerkei.ru>
2021-02-05 15:45:40 +0300

    JS

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+let menu = document.getElementById("welcome-inner-menu");
+// menu.style.background = '#000'
+// menu.style.background = 'transparent';
+console.log(window);
+console.log(window.getComputedStyle(tours));
